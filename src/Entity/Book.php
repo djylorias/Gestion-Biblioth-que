27258a650881dbf0655bf5afa3973263410016fd
Bernard ludovic <ludovic.bernard.etu@univ-lille.fr>
2025-08-31 18:33:53 +0200

    Create User and Book migrations

---
+++ src/Entity/Book.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $synopsis = null;
+
+    #[ORM\Column(type: Types::SMALLINT)]
+    private ?int $nb_pages = null;
+
+    #[ORM\ManyToOne(inversedBy: 'books')]
+    private ?User $is_borrowed = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(?string $synopsis): static
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getNbPages(): ?int
+    {
+        return $this->nb_pages;
+    }
+
+    public function setNbPages(int $nb_pages): static
+    {
+        $this->nb_pages = $nb_pages;
+
+        return $this;
+    }
+
+    public function getIsBorrowed(): ?User
+    {
+        return $this->is_borrowed;
+    }
+
+    public function setIsBorrowed(?User $is_borrowed): static
+    {
+        $this->is_borrowed = $is_borrowed;
+
+        return $this;
+    }
+}
