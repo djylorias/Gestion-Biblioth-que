20e89d7863cd0d05f73eb1e26f6726f096ef8990
Bernard ludovic <ludovic.bernard.etu@univ-lille.fr>
2025-09-09 18:32:09 +0200

    Fix subscriber form wrong labels

--- src/Form/SubscriberType.php
+++ src/Form/SubscriberType.php
@@ -8,15 +8,21 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 
 class SubscriberType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('email')->add('Sauvegarder', SubmitType::class,[
+            ->add('firstname', null, [
+                'label' => 'Prénom',
+            ])
+            ->add('lastname', null, [
+                'label' => 'Nom',
+            ])
+            ->add('email', EmailType::class)
+            ->add('Sauvegarder', SubmitType::class,[
                 'attr' => ['class' => 'float-left mr-2']
             ])
         ;
